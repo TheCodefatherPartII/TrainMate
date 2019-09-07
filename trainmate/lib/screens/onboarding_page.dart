31db39580f9d150f62fcd50fc602831b79a76ad5
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-09-07 17:16:59 +1000

    Added placeholder logo.

--- trainmate/lib/screens/onboarding_page.dart
+++ trainmate/lib/screens/onboarding_page.dart
@@ -45,11 +45,6 @@ class _OnboardingPageState extends State<OnboardingPage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
         body: Center(
           // Center is a layout widget. It takes a single child and positions it
           // in the middle of the parent.
@@ -70,6 +65,13 @@ class _OnboardingPageState extends State<OnboardingPage> {
             // horizontal).
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              new Container(
+                child: new Image.asset(
+                  'images/logo.png',
+                  height: 60.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
               new Text("Hello", style: TextStyle(fontSize: 28)),
               Padding(
                 padding: EdgeInsets.all(20.0),
