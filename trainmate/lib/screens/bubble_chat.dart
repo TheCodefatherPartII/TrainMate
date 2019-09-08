452508d68926c820abc770933a88490d7c1f2327
Mahesh Chauhan <mahesh.chauhan@terem.com.au>
2019-09-08 11:43:26 +1000

    updated icon for broadcast

--- trainmate/lib/screens/bubble_chat.dart
+++ trainmate/lib/screens/bubble_chat.dart
@@ -13,10 +13,13 @@ class Bubble extends StatelessWidget {
     final bg = isBroadcast
         ? Theme.of(context).primaryColor
         : (isMe ? Colors.white : Colors.greenAccent.shade100);
-    final align = isBroadcast ? CrossAxisAlignment.stretch : isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start;
+    final align = isBroadcast
+        ? CrossAxisAlignment.stretch
+        : isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start;
     final icon = delivered ? Icons.done_all : Icons.done;
     final textColor = isBroadcast ? Colors.white : Colors.black38;
-    final metaTextColor = isBroadcast ? Colors.white.withOpacity(0.75) : Colors.black38;
+    final metaTextColor =
+        isBroadcast ? Colors.white.withOpacity(0.75) : Colors.black38;
 
     final radius = isBroadcast
         ? BorderRadius.circular(10.0)
@@ -52,11 +55,23 @@ class Bubble extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.only(right: 48.0),
-                child: Text(message,
-                    style: TextStyle(
-                      color: textColor,
-                      fontSize: 15.0,
-                    )),
+                child: Row(children: <Widget>[
+                  isBroadcast
+                      ? Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                          size: 30,
+                        )
+                      : Container(),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(message,
+                        style: TextStyle(
+                          color: textColor,
+                          fontSize: 15.0,
+                        )),
+                  )
+                ]),
               ),
               Positioned(
                 top: -2.0,
@@ -68,14 +83,14 @@ class Bubble extends StatelessWidget {
                     )),
               ),
               Positioned(
-                  top: 8.0,
-                  right: 0.0,
-                  child: Icon(
-                      icon,
-                      size: 12.0,
-                      color: metaTextColor,
-                    ),
-                  )
+                top: 8.0,
+                right: 0.0,
+                child: Icon(
+                  icon,
+                  size: 12.0,
+                  color: metaTextColor,
+                ),
+              )
             ],
           ),
         )
